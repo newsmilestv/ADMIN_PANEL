87fd8492509523e4e96cde931c09bfe0edde9be1
Leonid First Comp <dil_zid@mail.ru>
2022-02-01 20:31:55 +0300

    Complete index.php

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 
 require __DIR__ . '/autoload.php';
 
+use App\Controllers\Index;
+use App\Exceptions\DbConnectEx;
+use App\Exceptions\SqlException;
 
 try {
     $ctrl = $_GET['ctrl'] ?? 'Index';
     $class = '\App\Controllers\\' . $ctrl;
     $ctrl = new $ctrl;
     $ctrl();
-} catch (\Throwable $th) {
-    //throw $th;
+} catch (SqlException $ex) {
+    $ctrl = new \App\Controllers\SqlError($ex);
+    $crtl();
+} catch (DbConnectEx $ex) {
+    $ctrl = new \App\Controllers\DbError($ex);
+    $ctrl();
+} catch (\Exception $ex) {
+    echo 'Неизвестная ошибка!!!' . $ex->getMessage();
 }
