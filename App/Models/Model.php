a9ae86b406fa42747c82e5a4a3bda4ca916ed20b
Leonid First Comp <dil_zid@mail.ru>
2022-02-07 21:53:01 +0300

    Add functions findAll() and findById() in App/Models/Model:

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -16,9 +16,24 @@ abstract class Model
 
      public static function findAll() : array
      {
-        $db = Db::instance();
-        $class = static::class;
-        $sql = 'SELECT * FROM ' . static::TABLE;
-        return $db->query($class, $sql, []);
+         $db = Db::instance();
+         $class = static::class;
+         $sql = 'SELECT * FROM ' . static::TABLE;
+         return $db->query($class, $sql, []);
      }
+
+     /**
+      * Функция findById($id) возвращает одну запись из таблицы с заданным id
+      * @param $id integer
+      * @return bool | object
+      */
+      public static function findById($id) : int
+      {
+         $db = Db::instance();
+         $class = static::class;
+         $sql = 'SELECT * FROM ' . static::TABLE . ' WHERE id=:id';
+         $data = [':id' => $id];
+         $value = $db->query($class, $sql, $data);
+         return $value ? $value[0] : null;
+      }
 }
