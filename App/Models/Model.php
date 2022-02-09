26bb42539e1c5ed4759816a52f7bd498ee1aa2c1
Leonid Second Comp <li_che_in@mail.ru>
2022-02-09 14:08:05 +0300

    Start create public function insert() App/Models/Model:

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -36,4 +36,32 @@ abstract class Model
          $value = $db->query($class, $sql, $data);
          return $value ? $value[0] : null;
       }
+      /**
+       * Функция проверяет есть ли значение в переменной $id
+       */
+      public function isNew() : bool
+      {
+         return empty($this->id);
+      }
+      /**
+       * Функция вставляет новую запись
+       */
+      public function insert()
+      {
+         if (!$this->isNew()) {
+            return;
+         }
+
+         $columns = [];
+         $values = [];
+
+         foreach ($this as $key => $value) {
+            if ('id' == $key) {
+               continue;
+            }
+
+            $columns[] = $key;
+            $values[':' . $key] = $value;
+         }        
+      }
 }
