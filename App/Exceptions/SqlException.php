061e9d336b632ea1bba198fb3e85c7f019cc1bde
Leonid First Comp <dil_zid@mail.ru>
2022-02-21 19:56:08 +0300

    Add code in App/Exceptions/SqlException:

---
+++ App/Exceptions/SqlException.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Exceptions;
+
+use Throwable;
+
+class SqlException extends \Exception
+{
+    protected $query;
+
+    public function __construct(
+        $query, 
+        $message = '', 
+        $code = 0,
+        Throwable $previous = null,
+        $sql = ''
+    )
+    {
+        $this->query = $query;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getQuery()
+    {
+        return $this->query;
+    }
+}
