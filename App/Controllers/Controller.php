3c866d2d68ee00d2976948aec3bab82be631bcc3
Leonid First Comp <dil_zid@mail.ru>
2022-03-11 18:48:11 +0300

    Add code in App/Controllers/Controller:

---
+++ App/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Controllers;
+
+use App\View\View;
+
+abstract class Controller
+{
+    protected $vew;
+    abstract protected function handle();
+
+    public function __construct()
+    {
+        $this->view = new View;
+    }
+
+    protected function access() : bool
+    {
+        return true;
+    }
+
+    public function __invoke()
+    {
+        if ($this->access()) {
+            $this->handle();
+        } else {
+            die('Нет доступа!!!');
+        }
+    }
+}
