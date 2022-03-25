14522980df9fa84be459cb0161a3fc6f60388419
Leonid Second Comp <li_che_in@mail.ru>
2022-03-25 12:33:09 +0300

    Add code in App/View/templates/admin/create:

---
+++ App/View/templates/admin/create.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CREATE</title>
+</head>
+
+<body>
+    <h2>Добавьте новость!!!</h2>
+
+    <form action="/?ctrl=Save&id=article&id" method="post">
+        <p>
+            <input type="text" name="title" size="77" placeholder="Заголовок статьи!!!" required>
+        </p>
+
+        <p>
+            <textarea name="content" cols="80" rows="20" placeholder="Текст!!!" required></textarea>
+        </p>
+
+        <p>
+            <input type="text" name="author" placeholder="Автор статьи!!!" required>
+        </p>
+
+        <button type="submit">Добавить</button>
+    </form>
+</body>
+
+</html>
