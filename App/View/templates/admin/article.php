a121b80495beca5d99f8b96ec2cc487c47dc94c2
Leonid Second Comp <li_che_in@mail.ru>
2022-03-25 13:20:36 +0300

    Add little changes in App/View/templates/admin/article:

--- App/View/templates/admin/article.php
+++ App/View/templates/admin/article.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ARTICLE</title>
 </head>
+
 <body>
+    <p align="right">
+        <a href="./?ctrl=Admin" style="font-weight: bold; font-size: large; color: palevioletred">
+            Админ Панель
+        </a>
+    </p>
 
     <h2><?= $article->title; ?></h2>
     <hr>
     <p><?= $article->content; ?></p>
 
-    <?php 
-        if (!empty($article->author)) {
-            echo 'Автор: ' . $article->author;
-        } else {
-            echo 'Автор: Без Автора!!!';
-        }
+    <?php
+    if (!empty($article->author)) {
+        echo 'Автор: ' . $article->author;
+    } else {
+        echo 'Автор: Без Автора!!!';
+    }
     ?>
     <hr>
 
@@ -30,4 +37,5 @@
         </a>
     </p>
 </body>
+
 </html>
