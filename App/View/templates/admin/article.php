2501cd372d875e8de32d5cb2e91824456f765e37
Leonid Second Comp <li_che_in@mail.ru>
2022-03-25 12:15:42 +0300

    Add code in App/View/templates/admin/article:

---
+++ App/View/templates/admin/article.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARTICLE</title>
+</head>
+<body>
+
+    <h2><?= $article->title; ?></h2>
+    <hr>
+    <p><?= $article->content; ?></p>
+
+    <?php 
+        if (!empty($article->author)) {
+            echo 'Автор: ' . $article->author;
+        } else {
+            echo 'Автор: Без Автора!!!';
+        }
+    ?>
+    <hr>
+
+    <p align="center">
+        <a href="./?ctrl=Delete&id=<?= $article->id; ?>" style="color: red;">
+            <b>Удалить</b>
+        </a>
+        <a href="./?ctrl=Update&id=<?= $article->id; ?>" style="color: yellowgreen;">
+            <b>Редактировать</b>
+        </a>
+    </p>
+</body>
+</html>
